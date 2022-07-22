1e81286e49bc14af7e2a68ab120f76fead6998c2
Serg <64j@mail.ru>
2022-07-22 17:57:28 +0300

    up

--- assets/plugins/vue-manager/resourses/src/utils/http.js
+++ assets/plugins/vue-manager/resourses/src/utils/http.js
@@ -66,7 +66,7 @@ export default {
       method: method,
       body: this.setBody(body || ''),
       headers: this.setHeaders(),
-      credentials: 'include'
+      credentials: 'same-origin'
     }).then(this.handlerResponse).catch(this.handlerCatch)
   },
 
@@ -105,7 +105,7 @@ export default {
     return fetch(this.baseUrl + 'manager/processors/login.processor.php', {
       method: 'post',
       body: body,
-      //credentials: 'include'
+      credentials: 'same-origin'
     }).then((res) => {
       if (res.status === 404) {
         return {
